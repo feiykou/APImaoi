83e4aefc30c16965471c57cb41c1f2d9c34cfc60
feiy <feiy@163.com>
2018-12-26 14:15:07 +0800

    唔该

--- application/api/service/WxNotify.php
+++ application/api/service/WxNotify.php
@@ -9,7 +9,6 @@
 namespace app\api\service;
 use app\api\model\ProductStock;
 use app\lib\enum\OrderStatusEnum;
-use app\lib\exception\TokenException;
 use think\Db;
 use think\Exception;
 use think\Loader;
@@ -21,47 +20,47 @@ Loader::import('WxPay.WxPay', EXTEND_PATH, '.Api.php');
 
 class WxNotify extends \WxPayNotify
 {
-    public function NotifyProcess($objData, $config, &$msg){
-        $data = $objData->GetValues();
-        if($data['result_code'] == 'SUCCESS'){
-            $orderNo = $data['out_trade_no'];
-            Db::startTrans();
-            try{
-                // 检查库存，并更新订单状态
-                $order = OrderModel::where('order_no','=',$orderNo)
-                    ->lock(true)
-                    ->find();
-                if($order->status == OrderStatusEnum::UNPAID){
-                    $service = new OrderService();
-                    $status = $service->checkOrderStock($order->id);
-                    if($status['pass']){
-                        $this->updateOrderStock($order->id, true);
-                        $this->reduceStock($status);
-                    }else{
-                        $this->updateOrderStock($order->id, false);
-                    }
-                }
-                Db::commit();
-
-            }catch (Exception $ex){
-                Db::rollback();
-                Log::error($ex);
-                return false;
-            }
-            return true;
-        }
-    }
-
-    private function reduceStock($status){
-        foreach ($status['pStatusArray'] as $singlePStatus){
-            ProductStock::where('id','=',$singlePStatus['stockId'])
-                ->setDec('stock_num',$singlePStatus['count']);
-        }
-    }
-
-    private function updateOrderStock($orderID, $success){
-        $status = $success?OrderStatusEnum::PAID : OrderStatusEnum::PAID_BUT_OUT_OF;
-        OrderModel::where('id','=',$orderID)
-            ->update(['status'=>$status]);
-    }
+//    public function NotifyProcess($objData, $config, &$msg){
+//        $data = $objData->GetValues();
+//        if($data['result_code'] == 'SUCCESS'){
+//            $orderNo = $data['out_trade_no'];
+//            Db::startTrans();
+//            try{
+//                // 检查库存，并更新订单状态
+//                $order = OrderModel::where('order_no','=',$orderNo)
+//                    ->lock(true)
+//                    ->find();
+//                if($order->status == OrderStatusEnum::UNPAID){
+//                    $service = new OrderService();
+//                    $status = $service->checkOrderStock($order->id);
+//                    if($status['pass']){
+//                        $this->updateOrderStock($order->id, true);
+//                        $this->reduceStock($status);
+//                    }else{
+//                        $this->updateOrderStock($order->id, false);
+//                    }
+//                }
+//                Db::commit();
+//
+//            }catch (Exception $ex){
+//                Db::rollback();
+//                Log::error($ex);
+//                return false;
+//            }
+//            return true;
+//        }
+//    }
+//
+//    private function reduceStock($status){
+//        foreach ($status['pStatusArray'] as $singlePStatus){
+//            ProductStock::where('id','=',$singlePStatus['stockId'])
+//                ->setDec('stock_num',$singlePStatus['count']);
+//        }
+//    }
+//
+//    private function updateOrderStock($orderID, $success){
+//        $status = $success?OrderStatusEnum::PAID : OrderStatusEnum::PAID_BUT_OUT_OF;
+//        OrderModel::where('id','=',$orderID)
+//            ->update(['status'=>$status]);
+//    }
 }
